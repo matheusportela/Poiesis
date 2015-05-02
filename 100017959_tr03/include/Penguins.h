38462b12189289ad1e0c56d1f65974af8338da65
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2015-05-02 00:24:14 -0300

    Penguins move with user input
    Penguins can move with W, A, S, D input. However, W and S only increases/decreases acceleration. The displacement should consider current rotation.

--- 100017959_tr03/include/Penguins.h
+++ 100017959_tr03/include/Penguins.h
@@ -20,19 +20,54 @@
 #include "GameObject.h"
 #include "InputManager.h"
 #include "Sprite.h"
+#include "Vector.h"
+
+// Remove after test
+#include "Logger.h"
 
 class Penguins : public GameObject
 {
   public:
+    // Initializes Penguins in the given position.
     Penguins(Point position);
+
+    // Destroys Penguins.
     ~Penguins();
+
+    // Updates Penguins position after some elapsed time.
+    void UpdatePosition(float dt);
+
+    // Runs update cycle.
     void Update(float dt);
+
+    // Renders Penguins.
     void Render();
+
+    // Checks whether Penguins is dead.
     bool IsDead();
+
+    // Shoots in the current mouse position.
     void Shoot();
+
+    // Changes Penguins speed given an acceleration.
+    void ChangeSpeed(float acceleration);
+
+    // Increases Penguins speed.
+    void SpeedUpCallback();
+
+    // Decreases Penguins speed.
+    void SlowDownCallback();
+
+    // Turns Penguins to the left.
+    void LeftRotationCallback();
+
+    // Turns Penguins to the right.
+    void RightRotationCallback();
 
   private:
     int hp;
+    float linearAcceleration;
+    float angularSpeed;
     Sprite* bodySprite;
     Sprite* cannonSprite;
 };
--- 100017959_tr03/src/Penguins.cpp
+++ 100017959_tr03/src/Penguins.cpp
@@ -8,9 +8,21 @@
 
 Penguins::Penguins(Point position)
 {
+    hp = CFG_GETI("PENGUINS_HP");
+    linearAcceleration = CFG_GETF("PENGUINS_ACCELERATION");
+    angularSpeed = CFG_GETF("PENGUINS_ANGULAR_SPEED");
     bodySprite = new Sprite(CFG_GETP("PENGUINS_BODY_SPRITE"));
     cannonSprite = new Sprite(CFG_GETP("PENGUINS_CANNON_SPRITE"));
     box.SetCenter(position, bodySprite->GetWidth(), bodySprite->GetHeight());
+
+    REGISTER_INPUT_KEY_CALLBACK(Penguins::SpeedUpCallback,
+        InputType::KeyDown, KeyboardButton::LowercaseW);
+    REGISTER_INPUT_KEY_CALLBACK(Penguins::SlowDownCallback,
+        InputType::KeyDown, KeyboardButton::LowercaseS);
+    REGISTER_INPUT_KEY_CALLBACK(Penguins::LeftRotationCallback,
+        InputType::KeyDown, KeyboardButton::LowercaseA);
+    REGISTER_INPUT_KEY_CALLBACK(Penguins::RightRotationCallback,
+        InputType::KeyDown, KeyboardButton::LowercaseD);
 }
 
 Penguins::~Penguins()
@@ -19,17 +31,27 @@ Penguins::~Penguins()
     delete bodySprite;
 }
 
+void Penguins::UpdatePosition(float dt)
+{
+    Vector displacement = speed;
+    displacement.Multiply(dt);
+    
+    Point position = GetCenter();
+    position.Add(displacement);
+    box.SetCenter(position, bodySprite->GetWidth(), bodySprite->GetHeight());
+}
 
 void Penguins::Update(float dt)
 {
-
+    LOG_D("[Penguins] Speed: " << speed.ToString());
+    UpdatePosition(dt);
 }
 
 void Penguins::Render()
 {
     Point renderPoint = Camera::WorldToScreenPoint(box.GetPoint());
-    bodySprite->Render(renderPoint);
-    cannonSprite->Render(renderPoint);
+    bodySprite->Render(renderPoint, rotation);
+    cannonSprite->Render(renderPoint, rotation);
 }
 
 bool Penguins::IsDead()
@@ -40,4 +62,35 @@ bool Penguins::IsDead()
 void Penguins::Shoot()
 {
 
+}
+
+void Penguins::ChangeSpeed(float acceleration)
+{
+    Vector deltaSpeed;
+
+    deltaSpeed.Set(1, 0);
+    deltaSpeed.Normalize();
+    deltaSpeed.Multiply(acceleration);
+
+    speed.Add(deltaSpeed);
+}
+
+void Penguins::SpeedUpCallback()
+{
+    ChangeSpeed(linearAcceleration);
+}
+
+void Penguins::SlowDownCallback()
+{
+    ChangeSpeed(-linearAcceleration);
+}
+
+void Penguins::LeftRotationCallback()
+{
+    rotation += angularSpeed;
+}
+
+void Penguins::RightRotationCallback()
+{
+    rotation -= angularSpeed;
 }

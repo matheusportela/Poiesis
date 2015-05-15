620e926903b487a0d44e0ef88784ef46f2552e3a
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2015-05-15 16:38:50 -0300

    Creating log functionality

---
+++ include/bandit/core/Log.h
@@ -0,0 +1,78 @@
+// Logging functionality.
+//
+// Logs can be generated in four different levels: debug, info, warning and
+// error. A log level is set to configure the minimum level that will be
+// displayed on screen.
+
+#ifndef LOG_H_
+#define LOG_H_
+
+#include <iostream>
+#include <string>
+
+// Configures to display logs with level higher or equal to the given one.
+#define LOG_SET_DEBUG() Log::level = Log::Debug;
+#define LOG_SET_INFO() Log::level = Log::Info;
+#define LOG_SET_WARNING() Log::level = Log::Warning;
+#define LOG_SET_ERROR() Log::level = Log::Error;
+
+// Outputs a log message if it's level is high enough.
+#define LOG(level, message) \
+    do { \
+        if (level >= Log::level) { \
+            Log::GetStream() \
+            << Log::GetPrefix(level) \
+            << message \
+            << Log::GetSufix(); \
+        } \
+    } while (false)
+
+// Outputs debug log message.
+#define LOG_D(message) LOG(Log::Debug, message)
+
+// Outputs informational log message.
+#define LOG_I(message) LOG(Log::Info, message)
+
+// Outputs warning log message.
+#define LOG_W(message) LOG(Log::Warning, message)
+
+// Outputs error log message.
+#define LOG_E(message) LOG(Log::Error, message)
+
+class Log
+{
+  public:
+    // Possible log levels to filter which log message will be displayed.
+    enum LogLevel
+    {
+        Debug,
+        Info,
+        Warning,
+        Error,
+    };
+
+    // Getters and setters.
+    static LogLevel GetLevel();
+    static void SetLevel(LogLevel level);
+
+    // Gets the stream where messages will be displayed.
+    static std::ostream& GetStream() { return std::cerr; };
+
+    // Maps log levels to strings for printing purposes.
+    static std::string GetLevelString(LogLevel level);
+
+    // Maps log levels to colors for printing purposes.
+    static std::string GetLevelColor(LogLevel level);
+
+    // Formats a prefix for the log message.
+    static std::string GetPrefix(LogLevel level);
+
+    // Formats a sufix for the log message.
+    static std::string GetSufix();
+
+  private:
+    // Current log level.
+    static LogLevel level;
+};
+
+#endif // LOG_H_
---
+++ src/bandit/core/Log.cpp
@@ -0,0 +1,51 @@
+#include "bandit/core/Log.h"
+
+Log::LogLevel Log::level = Log::Info;
+
+std::string Log::GetLevelString(LogLevel level)
+{
+    switch (level)
+    {
+        case Debug:
+            return "D";
+        case Info:
+            return "I";
+        case Warning:
+            return "W";
+        case Error:
+            return "E";
+    }
+
+    std::cerr << "ERROR [Log] Log level \"" << level << "\" undefined."
+              << std::endl;
+    exit(1);
+}
+
+std::string Log::GetLevelColor(LogLevel level)
+{
+    switch (level)
+    {
+        case Debug:
+            return "\e[1;35m"; // purple
+        case Info:
+            return "\e[1;1m"; // white
+        case Warning:
+            return "\e[1;33m"; // yellow
+        case Error:
+            return "\e[1;31m"; // red
+    }
+
+    std::cerr << "ERROR [Log] Log level \"" << level << "\" undefined."
+              << std::endl;
+    exit(1);
+}
+
+std::string Log::GetPrefix(LogLevel level)
+{
+    return GetLevelColor(level) + GetLevelString(level) + ": ";
+}
+
+std::string Log::GetSufix()
+{
+    return "\e[0;0m\n";
+}

dfcb41469cce1dbe278732e6be0cde9313601bae
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2015-05-17 00:45:00 -0300

    Providing random number generation facilities

--- include/bandit/core/Random.h
+++ include/bandit/core/Random.h
@@ -6,10 +6,25 @@
 #include <ctime>
 #include <cstdlib>
 
+#include "bandit/core/Log.h"
+
 class Random
 {
   public:
     Random();
+
+    // Generates random integer from 0 to RAND_MAX (at least 32767).
+    int GenerateInt();
+
+    // Generates random integer from minimum to maximum values, not inclusive.
+    int GenerateInt(int min, int max);
+
+    // Generates random real number from 0 to 1.
+    float GenerateFloat();
+
+    // Generates random real number from minimum to maximum values, not
+    // inclusive.
+    float GenerateFloat(float min, float max);
 
   private:
     void SeedRandomNumberGenerator();
--- src/bandit/core/Random.cpp
+++ src/bandit/core/Random.cpp
@@ -9,3 +9,35 @@ void Random::SeedRandomNumberGenerator()
 {
     srand(time(NULL));
 }
+
+int Random::GenerateInt()
+{
+    return rand();
+}
+
+int Random::GenerateInt(int min, int max)
+{
+    if (max <= min)
+    {
+        LOG_E("[Random] Maximum value must be greater than minimum value.");
+        exit(1);
+    }
+
+    return (min + rand()%(max - min));
+}
+
+float Random::GenerateFloat()
+{
+    return GenerateFloat(0, 1);
+}
+
+float Random::GenerateFloat(float min, float max)
+{
+    if (max <= min)
+    {
+        LOG_E("[Random] Maximum value must be greater than minimum value.");
+        exit(1);
+    }
+
+    return (min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX/(max-min))));
+}

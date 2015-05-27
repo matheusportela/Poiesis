0b730feef6f9abb40e42c3feb96a9a52cf2efde7
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2015-05-26 22:52:40 -0300

    Creating only a single cell for testing purposes

--- src/poiesis/levels/Level1.cpp
+++ src/poiesis/levels/Level1.cpp
@@ -5,17 +5,7 @@ void Level1::Start()
     LOG_I("[Level1] Starting");
     
     EntityFactory::CreateBackground();
-
-    Random r;
-    float x;
-    float y;
-
-    for (int i = 0; i < 50; ++i)
-    {
-        x = r.GenerateFloat(0, 720);
-        y = r.GenerateFloat(0, 1280);
-        EntityFactory::CreateCell(1, Vector(x, y));
-    }
+    EntityFactory::CreateCell(1, Vector(400, 300));
 
     Engine::GetInstance().AddSystem(std::make_shared<InputSystem>());
     Engine::GetInstance().AddSystem(std::make_shared<ParticleSystem>());

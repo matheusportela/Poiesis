4d91075d72848fd774891300863d4750fa293c8e
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2015-07-06 16:00:31 -0300

    Deleting cell component since it isn't necessary with complexity component.

--- include/poiesis/EntityFactory.h
+++ include/poiesis/EntityFactory.h
@@ -11,7 +11,6 @@
 #include "poiesis/components/ButtonComponent.h"
 #include "poiesis/components/CameraComponent.h"
 #include "poiesis/components/CameraFollowComponent.h"
-#include "poiesis/components/CellComponent.h"
 #include "poiesis/components/CellParticleComponent.h"
 #include "poiesis/components/ColliderComponent.h"
 #include "poiesis/components/CombatComponent.h"
